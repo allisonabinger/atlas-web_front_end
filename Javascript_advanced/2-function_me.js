fbd9a4580b13835681efc9ccec46baac83fc55fc
Allison <allisonabinger@gmail.com>
2024-03-12 16:14:56 -0500

    A - 03/12 04:14 PM - Task 2 - function returns a new function that can be called later.

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,2 +1,11 @@
 // welcomeMessage accepts one argument fullName, which creates a closure for an alert displaying "Welcome <fullname>"
 
+function welcomeMessage(fullName) {
+	return function() {
+		alert("Welcome " + fullName);
+	}
+}
+
+var guillaume = welcomeMessage("Guillaume");
+var alex = welcomeMessage("Alex");
+var fred = welcomeMessage("Fred");
